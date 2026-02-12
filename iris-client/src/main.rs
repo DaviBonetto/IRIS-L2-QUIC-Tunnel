d866b9d4be1391fb58861f5442482a080c4f5988
Davi Bonetto <davi.bonetto100@gmail.com>
2026-02-12 18:26:09 -0300

    feat(client): send payload

--- iris-client/src/main.rs
+++ iris-client/src/main.rs
@@ -2,13 +2,16 @@
 use anyhow::Result;
 use quinn::Endpoint;
 use tracing::info;
+use tokio::io::AsyncWriteExt;
 #[tokio::main]
 async fn main() -> Result<()> {
     tracing_subscriber::fmt::init();
     let mut endpoint = Endpoint::client("0.0.0.0:0".parse()?)?;
     endpoint.set_default_client_config(config::configure_client());
     let conn = endpoint.connect("127.0.0.1:4433".parse()?, "localhost")?.await?;
-    let (mut send, mut recv) = conn.open_bi().await?;
-    info!("Stream opened");
+    let (mut send, _recv) = conn.open_bi().await?;
+    send.write_all(b"Hello from Titan").await?;
+    send.finish().await?;
+    info!("Sent payload");
     Ok(())
 }
