aa90c7d2b85c1e7880dbade4a3cd74009f7e4358
Davi Bonetto <davi.bonetto100@gmail.com>
2026-02-12 18:26:10 -0300

    ci: simulation script

---
+++ simulate_v2.ps1
@@ -0,0 +1,8 @@
+﻿Write-Host "Building..."
+cargo build
+Write-Host "Starting Server..."
+$p = Start-Process cargo -ArgumentList "run -p iris-server" -PassThru -NoNewWindow
+Start-Sleep -Seconds 5
+Write-Host "Starting Client..."
+cargo run -p iris-client
+Stop-Process -Id $p.Id -Force
