e3c0eeaf66aeb050a9914802e09e6cbb9cf8ca2c
Davi Bonetto <davi.bonetto100@gmail.com>
2026-02-12 18:26:05 -0300

    feat(server): bind UDP socket

--- iris-server/src/main.rs
+++ iris-server/src/main.rs
@@ -1,3 +1,12 @@
 ﻿mod config;
+use anyhow::Result;
+use quinn::Endpoint;
+use tracing::info;
 #[tokio::main]
-async fn main() {}
+async fn main() -> Result<()> {
+    tracing_subscriber::fmt::init();
+    let config = config::configure_server()?;
+    let endpoint = Endpoint::server(config, "127.0.0.1:4433".parse()?)?;
+    info!("Listening on {}", endpoint.local_addr()?);
+    Ok(())
+}
