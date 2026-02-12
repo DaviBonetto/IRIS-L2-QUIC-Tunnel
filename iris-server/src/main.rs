63bd238f66b237d1dc41ce08fbd1b9c0246ef960
Davi Bonetto <davi.bonetto100@gmail.com>
2026-02-12 18:26:05 -0300

    feat(server): accept loop

--- iris-server/src/main.rs
+++ iris-server/src/main.rs
@@ -1,12 +1,20 @@
 ﻿mod config;
 use anyhow::Result;
 use quinn::Endpoint;
-use tracing::info;
+use tracing::{info, error};
 #[tokio::main]
 async fn main() -> Result<()> {
     tracing_subscriber::fmt::init();
     let config = config::configure_server()?;
-    let endpoint = Endpoint::server(config, "127.0.0.1:4433".parse()?)?;
-    info!("Listening on {}", endpoint.local_addr()?);
+    let endpoint = Endpoint::server(config, "0.0.0.0:4433".parse()?)?;
+    info!("Listening...");
+    while let Some(conn) = endpoint.accept().await {
+        tokio::spawn(async move {
+            match conn.await {
+                Ok(c) => info!("Connected: {}", c.remote_address()),
+                Err(e) => error!("Failed: {}", e),
+            }
+        });
+    }
     Ok(())
 }
