2aed2d37f8388ae34016ed8e4dffb606bce2512c
Davi Bonetto <davi.bonetto100@gmail.com>
2026-02-12 18:26:04 -0300

    feat(protocol): define ALPN constants

--- iris-common/src/lib.rs
+++ iris-common/src/lib.rs
@@ -4,3 +4,6 @@ pub fn make_server_cert() -> Result<(Vec<u8>, Vec<u8>), Box<dyn std::error::Erro
     let cert = generate_simple_self_signed(subject_alt_names)?;
     Ok((cert.serialize_der()?, cert.serialize_private_key_der()))
 }
+
+
+pub const ALPN_QUIC_HTTP: &[&[u8]] = &[b"hq-29"];
