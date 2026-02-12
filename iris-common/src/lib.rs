8d70be68f52c30a4dcd5e0b378bcf56b61afe268
Davi Bonetto <davi.bonetto100@gmail.com>
2026-02-12 18:26:03 -0300

    feat(security): implement self-signed certs

--- iris-common/src/lib.rs
+++ iris-common/src/lib.rs
@@ -1,1 +1,6 @@
-﻿// IRIS Common
+﻿use rcgen::generate_simple_self_signed;
+pub fn make_server_cert() -> Result<(Vec<u8>, Vec<u8>), Box<dyn std::error::Error + Send + Sync>> {
+    let subject_alt_names = vec!["localhost".to_string(), "127.0.0.1".to_string()];
+    let cert = generate_simple_self_signed(subject_alt_names)?;
+    Ok((cert.serialize_der()?, cert.serialize_private_key_der()))
+}
