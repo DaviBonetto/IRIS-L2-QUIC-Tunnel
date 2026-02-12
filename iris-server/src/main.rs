a2ea74424de41bc4e55ad799b12ee2a275801d21
Davi Bonetto <davi.bonetto100@gmail.com>
2026-02-12 18:26:06 -0300

    feat(server): bidirectional streams

--- iris-server/src/main.rs
+++ iris-server/src/main.rs
@@ -1,17 +1,25 @@
 ﻿mod config;
 use anyhow::Result;
-use quinn::Endpoint;
+use quinn::{Endpoint, Connection};
 use tracing::{info, error};
+
+async fn handle(conn: Connection) {
+    info!("Connected: {}", conn.remote_address());
+    while let Ok((mut send, mut recv)) = conn.accept_bi().await {
+        let _ = recv.read_to_end(1024*64).await.map(|d| info!("Received: {:?}", String::from_utf8_lossy(&d)));
+        let _ = send.write_all(b"Ack").await;
+        let _ = send.finish().await;
+    }
+}
 #[tokio::main]
 async fn main() -> Result<()> {
     tracing_subscriber::fmt::init();
     let config = config::configure_server()?;
     let endpoint = Endpoint::server(config, "0.0.0.0:4433".parse()?)?;
-    info!("Listening...");
     while let Some(conn) = endpoint.accept().await {
         tokio::spawn(async move {
             match conn.await {
-                Ok(c) => info!("Connected: {}", c.remote_address()),
+                Ok(c) => handle(c).await,
                 Err(e) => error!("Failed: {}", e),
             }
         });
