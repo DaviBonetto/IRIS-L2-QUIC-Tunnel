488aa2a4d1f4065b5ff9ad799bf7c5220216de12
Davi Bonetto <davi.bonetto100@gmail.com>
2026-02-12 18:26:09 -0300

    feat(client): handshake

--- iris-client/src/main.rs
+++ iris-client/src/main.rs
@@ -1,1 +1,13 @@
-﻿mod config; fn main() {}
+﻿mod config;
+use anyhow::Result;
+use quinn::Endpoint;
+use tracing::info;
+#[tokio::main]
+async fn main() -> Result<()> {
+    tracing_subscriber::fmt::init();
+    let mut endpoint = Endpoint::client("0.0.0.0:0".parse()?)?;
+    endpoint.set_default_client_config(config::configure_client());
+    let conn = endpoint.connect("127.0.0.1:4433".parse()?, "localhost")?.await?;
+    info!("Connected!");
+    Ok(())
+}
